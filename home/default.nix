a548ba4958838020232016a38298bb59ec4f2abb
Romain Viallard <romain.viallard@outlook.fr>
2019-07-01 20:36:14 +0200

    Add gnome default background

--- home/default.nix
+++ home/default.nix
@@ -2,6 +2,12 @@
 
 {
   home.file.".background-image".source = ./wallpapers/mountains-on-mars.png;
+
+  dconf.settings = {
+    "org/gnome/desktop/background" = {
+      "picture-uri" = "file:///home/romain/.background-image";
+    };
+  };
   
   programs = {
     home-manager.enable = true;
