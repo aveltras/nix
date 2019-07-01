70e5c3bf7dda4e36082b6c332aebf777256577f1
Romain Viallard <romain.viallard@outlook.fr>
2019-07-01 17:01:26 +0200

    Setting default wallpaper

--- home/default.nix
+++ home/default.nix
@@ -1,7 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  home.file.".background-image".source = ./wallpapers/mountains-on-mars.png;
+  
   programs = {
+    home-manager.enable = true;
     git = {
       enable = true;
       userName = "Romain Viallard";
